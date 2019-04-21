71c9293abe27388b85a9243c4a86dacfb78a762a
Garrett Yamada <me@garrettyamada.com>
2019-04-21 11:14:11 -0500

    More metadata, this should be a signed commit.

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,9 @@ description 'Installs/Configures PDQ Deploy'
 long_description 'Installs PDQ Deploy console in client mode and configures a connection to a PDQ Deploy Central Server.'
 version '1.0.0'
 
+source_url 'https://github.com/gyamada619/pdqdeploy'
+issues_url 'https://github.com/gyamada619/pdqdeploy/issues'
+
 supports 'windows'
 
 chef_version '>= 13.0'
